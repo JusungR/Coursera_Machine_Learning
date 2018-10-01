b073308b6d11c7c0b6661f78fb451b846fd64c5d
Ryuu <yhwsr92@gmail.com>
2018-10-01 19:54:06 +0900

    Signed-off-by: Ryuu <yhwsr92@gmail.com>

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -53,6 +53,19 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+ for i = 1:m
+  Xtrain = X(1:i,:);
+	ytrain = y(1:i);
+  
+  theta = trainLinearReg(Xtrain, ytrain, lambda);
+   
+   
+   J_train = linearRegCostFunction(Xtrain, ytrain, theta, 0);
+   J_val = linearRegCostFunction(Xval, yval, theta, 0);
+   
+   error_train(i) = J_train;
+   error_val(i) = J_val;
+ endfor
 
 
 
---
+++ ex6/test.m
@@ -0,0 +1,20 @@
+while ~isempty(email_contents)
+
+    % Tokenize and also get rid of any punctuation
+    [str, email_contents] = ...
+       strtok(email_contents, ...
+              [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+   
+    % Remove any non alphanumeric characters
+    str = regexprep(str, '[^a-zA-Z0-9]', '');
+
+    % Stem the word 
+    % (the porterStemmer sometimes has issues, so we use a try catch block)
+    try str = porterStemmer(strtrim(str)); 
+    catch str = ''; continue;
+    end;
+
+    % Skip the word if it is too short
+    if length(str) < 1
+       continue;
+    end
